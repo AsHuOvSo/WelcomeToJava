ed92d2e4efb88203c15cd55de904c5247fe8b5fa
AsHuOvSo <asta@avra.dk>
2020-09-08 10:49:36 +0200

    Initial commit

---
+++ src/WelcomeToJava.java
@@ -0,0 +1,9 @@
+public class WelcomeToJava {
+    //psvm start of program
+    public static void main(String[] args) {
+        //Printing three different lines of text
+        System.out.println("Welcome to Java.");
+        System.out.println("Learning Java Now.");
+        System.out.println("Programming is fun.");
+    }
+}
